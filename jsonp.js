2e0d2095fcd292bb09cc9e5057e647323fc84dcf
lyg <ygl@szmsmk.com.cn>
2017-06-21 17:53:49 +0800

    init

---
+++ jsonp.js
@@ -0,0 +1,14 @@
+callback({
+    "info": "success",
+    "bool": true,
+    "data": [
+        {
+            "name": "张三",
+            "age": 23
+        },
+        {
+            "name": "李四",
+            "age": 23
+        }
+    ]
+})
